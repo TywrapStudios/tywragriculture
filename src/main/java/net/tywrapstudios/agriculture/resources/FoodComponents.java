91ad1edeefe7d293d1920cc6e868af809c159064
TywrapStudios <tywrap.studio@gmail.com>
2024-11-11 01:10:51 +0100

    Made Food Stuff an Enum, and removed helper method from Fuels
    Took 30 seconds

--- src/main/java/net/tywrapstudios/agriculture/resources/FoodComponents.java
+++ src/main/java/net/tywrapstudios/agriculture/resources/FoodComponents.java
@@ -1,19 +1,15 @@
 package net.tywrapstudios.agriculture.resources;
 
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public enum FoodComponents {
-    BRIQUETTE_FOOD(new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 120), 1f).build()),
     CAKE_SLICE_FOOD(new FoodComponent.Builder().hunger(8).saturationModifier(0.2f).build()),
     STRAWBERRY(new FoodComponent.Builder().hunger(2).saturationModifier(0.2f).snack().build()),
     TOMATO(new FoodComponent.Builder().hunger(3).saturationModifier(0.3f).build()),
     CARROT_PURPLE(new FoodComponent.Builder().hunger(4).saturationModifier(0.3f).build()),
     CARROT_BLACK(new FoodComponent.Builder().hunger(4).saturationModifier(0.3f).build()),
-    CABBAGE(new FoodComponent.Builder().hunger(3).saturationModifier(0.2f).build()),
     SWEET_POTATO(new FoodComponent.Builder().hunger(5).saturationModifier(0.4f).build()),
-    PINEAPPLE(new FoodComponent.Builder().hunger(4).saturationModifier(0.4f).build()),
+    PINEAPPLE_SLICE(new FoodComponent.Builder().hunger(4).saturationModifier(0.4f).build()),
     WHITE_MELON(new FoodComponent.Builder().hunger(5).saturationModifier(0.4f).build()),
     ORANGE_MELON(new FoodComponent.Builder().hunger(5).saturationModifier(0.4f).build());
 
--- src/main/java/net/tywrapstudios/agriculture/resources/Fuels.java
+++ src/main/java/net/tywrapstudios/agriculture/resources/Fuels.java
@@ -2,15 +2,12 @@ package net.tywrapstudios.agriculture.resources;
 
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.item.Item;
-import net.tywrapstudios.agriculture.content.item.ItemRegistry;
+import net.tywrapstudios.agriculture.registry.ItemRegistry;
+import net.tywrapstudios.agriculture.util.logging.LoggingHandlers;
 
 public class Fuels {
-
-    public static void addFuelType(Item item, Integer value) {
-        FuelRegistry.INSTANCE.add(item, value);
-    }
-
     public static void register() {
-        addFuelType(ItemRegistry.BRIQUETTE.get(), 1800);
+        FuelRegistry.INSTANCE.add(ItemRegistry.BRIQUETTE.get(), 1800);
+        LoggingHandlers.debug("Fuels have been registered.");
     }
 }
