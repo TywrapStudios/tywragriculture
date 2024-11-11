c6649f39d5bf509fc37b264af0064af0e6678682
TywrapStudios <tywrap.studio@gmail.com>
2024-11-11 01:08:19 +0100

    Improved Config using Jankson
    Took 16 seconds

--- src/main/java/net/tywrapstudios/agriculture/config/Config.java
+++ src/main/java/net/tywrapstudios/agriculture/config/Config.java
@@ -2,7 +2,15 @@ package net.tywrapstudios.agriculture.config;
 
 import blue.endless.jankson.Comment;
 
+/**
+ * This is where the Configuration Options are defined, you can make use of {@link blue.endless.jankson.Comment} to add a // type comment to the file.
+ * <br/> Comments are allowed as the file is initialized as .json5 and not plain .json.
+ * <br/> This is also considerably nicer for us to manage.
+ * <br/>
+ * <br/> I'd love to transfer over this system to all our other mods.
+ */
 public class Config {
+    public String format_version = "a";
     @Comment("Several configurations for utility features.")
     public UtilConfig util_config = new UtilConfig();
     public static class UtilConfig {
@@ -14,8 +22,6 @@ public class Config {
     @Comment("Several configurations for in-world features.")
     public WorldConfig world_config = new WorldConfig();
     public static class WorldConfig {
-        @Comment("Whether to allow golden carrots to be grown.")
-        public boolean allow_golden_carrot_crops = true;
         @Comment("Whether to allow right-clicking to harvest crops.")
         public boolean right_click_harvest = true;
     }
--- src/main/java/net/tywrapstudios/agriculture/config/ConfigManager.java
+++ src/main/java/net/tywrapstudios/agriculture/config/ConfigManager.java
@@ -3,16 +3,21 @@ package net.tywrapstudios.agriculture.config;
 import blue.endless.jankson.Jankson;
 import blue.endless.jankson.JsonElement;
 import blue.endless.jankson.api.SyntaxError;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.tywrapstudios.agriculture.util.logging.LoggingHandlers;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 
+/**
+ * The manager makes/edits the config file, and reads (loads) from it as well.
+ * <br/>This is utilizing {@link blue.endless.jankson.Jankson}, which is a different method of adding Config than our other mods.
+ */
 public class ConfigManager {
-    /**
-     * The manager makes/edits the config file, and reads (loads) from it as well.
-     */
     private static final Jankson jankson = Jankson.builder().build();
     private static final File configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "tywragriculture.json5");
     public static Config config;
@@ -21,6 +26,7 @@ public class ConfigManager {
         if (configFile.exists()) {
             try {
                 config = jankson.fromJson(jankson.load(configFile), Config.class);
+                LoggingHandlers.info("[Config] Loaded configuration file.");
             } catch (IOException | SyntaxError e) {
                 e.printStackTrace();
                 LoggingHandlers.error("[Config] Error loading configuration file; using default values.");
@@ -28,6 +34,42 @@ public class ConfigManager {
             }
         } else {
             config = new Config();
+            LoggingHandlers.info("[Config] No configuration file found, created new one.");
+            LoggingHandlers.info("[Config] `.../config/tywragriculture.json5`.");
+            saveConfig();
+        }
+    }
+
+    public static void reloadConfig(CommandContext ctx) {
+        ServerCommandSource source = (ServerCommandSource) ctx.getSource();
+        if (configFile.exists()) {
+            try {
+                config = jankson.fromJson(jankson.load(configFile), Config.class);
+                LoggingHandlers.info("[Config] Reloaded configuration file.");
+                source.sendFeedback(() -> {
+                    return Text.literal("[Config] Reloaded configuration file.").formatted(Formatting.GRAY);
+                }, true);
+            } catch (IOException | SyntaxError e) {
+                e.printStackTrace();
+                LoggingHandlers.error("[Config] Error loading configuration file; using default values.");
+                source.sendFeedback(() -> {
+                    return Text.literal("[Config] Error loading configuration file; using default values.").formatted(Formatting.RED);
+                }, true);
+                config = new Config();
+            }
+        } else {
+            config = new Config();
+            LoggingHandlers.info("[Config] No configuration file found, created new one.");
+            LoggingHandlers.info("[Config] `.../config/tywragriculture.json5`.");
+            LoggingHandlers.warn("[Config] Note that this generally shouldn't be happening, a file should be made and available before your run!");
+            source.sendFeedback(() -> {
+                return Text.literal("""
+                        [Config] No configuration file found, created new one.
+                        [Config] `.../config/tywragriculture.json5`.""").formatted(Formatting.GRAY);
+            }, true);
+            source.sendFeedback(() -> {
+                return Text.literal("[Config] Note that this generally shouldn't be happening, a file should be made and available before your run!").formatted(Formatting.RED);
+            }, true);
             saveConfig();
         }
     }
