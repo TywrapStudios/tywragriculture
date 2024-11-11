bb16da2dc746c67b24ebf3c6d1815c2246eb72e9
TywrapStudios <tywrap.studio@gmail.com>
2024-11-11 01:08:57 +0100

    Additional Datagen stuff
    Took 10 seconds

--- src/main/java/net/tywrapstudios/agriculture/util/datagen/ModRecipeProvider.java
+++ src/main/java/net/tywrapstudios/agriculture/util/datagen/ModRecipeProvider.java
@@ -4,10 +4,12 @@ import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.util.Identifier;
+import net.tywrapstudios.agriculture.registry.ItemRegistry;
 
 import java.util.function.Consumer;
 
@@ -19,14 +21,11 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(Consumer<RecipeJsonProvider> exporter) {
-//        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_RUBY, 1)
-//                .pattern("SSS")
-//                .pattern("SRS")
-//                .pattern("SSS")
-//                .input('S', Items.STONE)
-//                .input('R', ModItems.RUBY)
-//                .criterion(hasItem(Items.STONE), conditionsFromItem(Items.STONE))
-//                .criterion(hasItem(ModItems.RUBY), conditionsFromItem(ModItems.RUBY))
-//                .offerTo(exporter, new Identifier(getRecipeName(ModItems.RAW_RUBY)));
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, ItemRegistry.PEAS.get(), 2)
+                .input(ItemRegistry.PEA_SHELL.get());
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, ItemRegistry.TOMATO_SEEDS.get(), 2)
+                .input(ItemRegistry.TOMATOES.get());
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.FOOD, ItemRegistry.TOMATO_SEEDS.get(), 2)
+                .input(ItemRegistry.ROTTEN_TOMATOES.get());
     }
 }
