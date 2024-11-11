2eb5faa1d0cf6144c00eaf305d5ce4b4046c366c
TywrapStudios <tywrap.studio@gmail.com>
2024-11-11 01:08:03 +0100

    Auto-add Cutouts for CropBlocks
    Took 16 seconds

--- src/client/java/net/tywrapstudios/TywragricultureClient.java
+++ src/client/java/net/tywrapstudios/TywragricultureClient.java
@@ -1,19 +1,26 @@
 package net.tywrapstudios;
 
+import com.tterrag.registrate.util.entry.RegistryEntry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.block.Block;
+import net.minecraft.block.CropBlock;
 import net.minecraft.client.render.RenderLayer;
-import net.tywrapstudios.agriculture.content.block.BlockRegistry;
+import net.minecraft.data.client.Models;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.tywrapstudios.agriculture.registry.BlockRegistry;
+
+import static net.tywrapstudios.agriculture.Tywragriculture.REGISTRATE;
 
 public class TywragricultureClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// Cutouts, this is to properly render crops.
-        BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(),
-				BlockRegistry.BLACK_CARROT.get(),
-				BlockRegistry.GOLD_CARROT.get(),
-				BlockRegistry.PURPLE_CARROT.get(),
-				BlockRegistry.TOMATO_PLANT.get()
-		);
+		for (RegistryEntry<Block> block : REGISTRATE.getAll(Registries.BLOCK.getKey())) {
+			if (block.get() instanceof CropBlock) {
+				BlockRenderLayerMap.INSTANCE.putBlock(block.get(), RenderLayer.getCutout());
+			}
+		}
 	}
 }
