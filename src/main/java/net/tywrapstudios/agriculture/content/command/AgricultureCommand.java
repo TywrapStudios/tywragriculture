ff39ff8c5a260c03c2f073c7aebd74930b5152ac
TywrapStudios <tywrap.studio@gmail.com>
2024-11-11 01:09:33 +0100

    Added the /agriculture command
    Took 14 seconds

---
+++ src/main/java/net/tywrapstudios/agriculture/content/command/AgricultureCommand.java
@@ -0,0 +1,81 @@
+package net.tywrapstudios.agriculture.content.command;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import net.tywrapstudios.agriculture.Tywragriculture;
+import net.tywrapstudios.agriculture.config.Config;
+import net.tywrapstudios.agriculture.config.ConfigManager;
+import net.tywrapstudios.agriculture.util.logging.LoggingHandlers;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class AgricultureCommand {
+
+    public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("agriculture")
+                .executes(AgricultureCommand::executePlain)
+                .then(CommandManager.literal("reload")
+                        .executes(AgricultureCommand::reload).requires((source) -> {
+                            return source.hasPermissionLevel(2);}))
+                .then(CommandManager.literal("dump_config")
+                        .executes(AgricultureCommand::dumpConfig)));
+    }
+
+    private static int executePlain(@NotNull CommandContext<ServerCommandSource> context) {
+        Config config = ConfigManager.config;
+
+        ServerCommandSource source = context.getSource();
+        boolean FD_LOADED = FabricLoader.getInstance().isModLoaded("farmersdelight");
+        String message = String.format("""
+                    ----[Tywragriculture]----
+                    > Mod Version: %s
+                    > Farmer's Delight loaded: %s
+                    -----------------------""", Tywragriculture.MOD_VERSION, FD_LOADED);
+        source.sendFeedback(() -> {
+            return Text.literal(message).formatted(Formatting.GOLD);
+        }, false);
+        return 1;
+    }
+
+    private static int reload(@NotNull CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
+        if (Objects.equals(ConfigManager.config.format_version, Tywragriculture.CONFIG_FORMAT)) {
+            source.sendFeedback(() -> {
+                return Text.literal("[Tywragriculture] Reloading!").formatted(Formatting.GRAY);
+            }, true);
+            ConfigManager.reloadConfig(context);
+        } else {
+            source.sendFeedback(() -> {
+                return Text.literal("[Tywragriculture] Could not reload Config: Format Version out of sync, please delete your config file and rerun Minecraft.") .formatted(Formatting.RED);
+            }, false);
+            LoggingHandlers.error("[Config] Your Config Version is out of Sync, please delete your config file and reload Minecraft.");
+        }
+        return 1;
+    }
+
+    private static int dumpConfig(@NotNull CommandContext<ServerCommandSource> context) {
+        ServerCommandSource source = context.getSource();
+        Config config = ConfigManager.config;
+        String message = String.format("""
+                    --------[Config]---------
+                    "format_version": "%s"
+                    "util_config": {
+                        "debug_mode": %s
+                        "suppress_warns": %s
+                    }
+                    "world_config": {
+                        "right_click_harvest": %s
+                    }
+                    -----------------------""", config.format_version, config.util_config.debug_mode, config.util_config.suppress_warns, config.world_config.right_click_harvest);
+        source.sendFeedback(() -> {
+            return Text.literal(message).formatted(Formatting.GRAY);
+        }, false);
+        return 1;
+    }
+}
