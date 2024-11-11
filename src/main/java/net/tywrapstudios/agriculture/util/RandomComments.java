abde302681a51b6c15513d3ba076bbc2c3824d2b
TywrapStudios <tywrap.studio@gmail.com>
2024-11-11 23:27:20 +0100

    Removed the shitty ass comments and start replacing them with phrases the community provides us!
    Took 48 seconds

--- src/main/java/net/tywrapstudios/agriculture/util/RandomComments.java
+++ src/main/java/net/tywrapstudios/agriculture/util/RandomComments.java
@@ -6,31 +6,24 @@ import java.util.Random;
 public class RandomComments {
 
     private static final List<String> INIT_PHRASES = List.of(
-            "Caught the baby bunny munching on my lettuce again... too cute to be mad!",
-            "Little scarecrow standing so proudly, scaring off all the birdies like a champ.",
-            "Farmer’s market? More like cuteness overload with all the tiny veggies lined up!",
-            "Somehow, the cows are the happiest in the rain; they’ve got that mud-glow!",
-            "Got a tiny, wiggly piglet today—cutest addition to the farm family.",
-            "The sunflowers keep trying to outgrow me; little show-offs!",
-            "Caught the sheep making a secret napping circle under the apple tree!",
-            "Watching ducklings paddle around is peak farm happiness.",
             "Do cows know how cute they look chewing on a daisy? Because they should.",
             "Every time I look at the baby pumpkins, my heart just grows a little bigger.",
-            "Chickens waddle-waddling after me like I'm their queen… farm royalty!",
             "Fawming Fwunny UwU",
-            "Baby goats have the fluffiest little tails, and it's not even fair!",
-            "The tomatoes are looking so plump, they almost make me wanna give 'em a name.",
-            "Ran out of carrots because the bunnies got to them first—again!",
-            "Just saw a lamb play-tag with a butterfly... hearts melting over here.",
-            "If plants could giggle, the peas would be in stitches!",
             "These zucchinis look like they're flexing; okay, I see you.",
-            "Horse tried to nibble my hat, and now it's officially a farm hat!",
-            "When the rooster crows, the little ducklings try to join in. It’s... a sound.",
-            "Harvested my first giant pumpkin, and I'm not saying it's my best friend, but..."
+            "When the rooster crows, the little ducklings try to join in. It’s... certainly a sound.",
+            "Harvested my first giant pumpkin, and I'm not saying it's my best friend, but hear me out...",
+            "Ah yes I love Config formatting version AAAA it reminds me of my mental health.",
+            "Dot put te fok in te micorwafe waltuh",
+            "Roses are red, violets are blue, unexpected \"{\" at line 32.",
+            "So like what the f*ck is a Tywrgaricultuerre?",
+            "garlic bread?",
+            "\"glorps glop bieb bobls\" ~ The Aliens saying this mod is fire",
+            "ORAJ POLE NA TRAKTORE HEY HOOP SJABADABA",
+            "Z-O-O-M, talk a lot on Twitter bish I'm not yo friend",
+            "I love it when the little piglets play together. So cute I could combust!"
     );
 
     public static String generateInitComment() {
-        Random random = new Random();
-        return INIT_PHRASES.get(random.nextInt(INIT_PHRASES.size()));
+        return INIT_PHRASES.get(new Random().nextInt(INIT_PHRASES.size()));
     }
 }
