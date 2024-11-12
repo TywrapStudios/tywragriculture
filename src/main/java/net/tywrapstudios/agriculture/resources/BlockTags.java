fe680d0e9612913d03eb3ab9bab5169bb4ea8855
TywrapStudios <tywrap.studio@gmail.com>
2024-11-13 00:16:28 +0100

    Block- and ItemTags
    Took 14 seconds

---
+++ src/main/java/net/tywrapstudios/agriculture/resources/BlockTags.java
@@ -0,0 +1,25 @@
+package net.tywrapstudios.agriculture.resources;
+
+import net.minecraft.block.Block;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.tywrapstudios.agriculture.Tywragriculture;
+
+public enum BlockTags {
+    KITCHEN(of("kitchen"));
+
+    private final TagKey<Block> blockTagKey;
+
+    BlockTags(TagKey<Block> blockTagKey) {
+        this.blockTagKey = blockTagKey;
+    }
+
+    public TagKey<Block> get() {
+        return blockTagKey;
+    }
+
+    private static TagKey<Block> of(String tagName) {
+        return TagKey.of(RegistryKeys.BLOCK, new Identifier(Tywragriculture.MOD_ID, tagName));
+    }
+}
---
+++ src/main/java/net/tywrapstudios/agriculture/resources/ItemTags.java
@@ -0,0 +1,29 @@
+package net.tywrapstudios.agriculture.resources;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
+import net.tywrapstudios.agriculture.Tywragriculture;
+
+public enum ItemTags {
+    KNIVES(of("knives")),
+    CONSUMABLES(of("consumables")),
+    DRINKS(of("drinks")),
+    FOODS(of("foods"));
+
+
+    private final TagKey<Item> itemTagKey;
+
+    ItemTags(TagKey<Item> itemTagKey) {
+        this.itemTagKey = itemTagKey;
+    }
+
+    public TagKey<Item> get() {
+        return itemTagKey;
+    }
+
+    private static TagKey<Item> of(String tagName) {
+        return TagKey.of(RegistryKeys.ITEM, new Identifier(Tywragriculture.MOD_ID, tagName));
+    }
+}
