a6e2bee901899532b0a0a65b871442cfbdbb5c73
TywrapStudios <tywrap.studio@gmail.com>
2024-11-14 00:00:40 +0100

    Small util stuff
    Took 1 minute

--- src/main/java/net/tywrapstudios/agriculture/util/datagen/ModLootTableProvider.java
+++ src/main/java/net/tywrapstudios/agriculture/util/datagen/ModLootTableProvider.java
@@ -2,8 +2,6 @@ package net.tywrapstudios.agriculture.util.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
-import net.minecraft.loot.condition.BlockStatePropertyLootCondition;
-import net.minecraft.loot.condition.LootCondition;
 
 public class ModLootTableProvider extends FabricBlockLootTableProvider {
     public ModLootTableProvider(FabricDataOutput dataOutput) {
--- src/main/java/net/tywrapstudios/agriculture/util/datagen/TywragricultureDataGenerator.java
+++ src/main/java/net/tywrapstudios/agriculture/util/datagen/TywragricultureDataGenerator.java
@@ -1,12 +1,7 @@
 package net.tywrapstudios.agriculture.util.datagen;
 
-import io.github.fabricators_of_create.porting_lib.data.ExistingFileHelper;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import net.fabricmc.loader.FabricLoader;
-import net.fabricmc.loader.impl.FabricLoaderImpl;
-import net.minecraft.resource.ResourceType;
-import net.tywrapstudios.agriculture.Tywragriculture;
 import net.tywrapstudios.agriculture.util.datagen.*;
 
 public class TywragricultureDataGenerator implements DataGeneratorEntrypoint {
