189c089c03966643dc847df4edc974e1409bb62e
TywrapStudios <tywrap.studio@gmail.com>
2024-11-15 00:17:13 +0100

    manually added GOLD_CROP for cutouts
    Took 2 hours 19 minutes

--- src/client/java/net/tywrapstudios/agriculture/TywragricultureClient.java
+++ src/client/java/net/tywrapstudios/agriculture/TywragricultureClient.java
@@ -7,6 +7,7 @@ import net.minecraft.block.Block;
 import net.minecraft.block.CropBlock;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.registry.Registries;
+import net.tywrapstudios.agriculture.registry.BlockRegistry;
 
 import static net.tywrapstudios.agriculture.Tywragriculture.REGISTRATE;
 
@@ -14,6 +15,7 @@ public class TywragricultureClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// Cutouts, this is to properly render crops.
+		BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), BlockRegistry.GOLD_CARROT);
 		for (RegistryEntry<Block> block : REGISTRATE.getAll(Registries.BLOCK.getKey())) {
 			if (block.get() instanceof CropBlock) {
 				BlockRenderLayerMap.INSTANCE.putBlock(block.get(), RenderLayer.getCutout());
