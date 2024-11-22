ce17104fd9d67538e7c3f0d800ea84560528748f
TywrapStudios <tywrap.studio@gmail.com>
2024-11-22 22:10:41 +0100

    Add a method for registering RecipeTypes and such

--- src/main/java/net/tywrapstudios/agriculture/recipe/Recipes.java
+++ src/main/java/net/tywrapstudios/agriculture/recipe/Recipes.java
@@ -1,5 +1,9 @@
 package net.tywrapstudios.agriculture.recipe;
 
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.recipe.Recipe;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.RecipeType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -8,9 +12,13 @@ import net.tywrapstudios.agriculture.recipe.recipes.MeatGrindingRecipe;
 
 public class Recipes {
     public static void registerRecipes() {
-        Registry.register(Registries.RECIPE_SERIALIZER, new Identifier(Tywragriculture.MOD_ID, MeatGrindingRecipe.Serializer.ID),
-                MeatGrindingRecipe.Serializer.INSTANCE);
-        Registry.register(Registries.RECIPE_TYPE, new Identifier(Tywragriculture.MOD_ID, MeatGrindingRecipe.Type.ID),
-                MeatGrindingRecipe.Type.INSTANCE);
+        registerRecipe(MeatGrindingRecipe.Serializer.ID, MeatGrindingRecipe.Type.ID, MeatGrindingRecipe.Serializer.INSTANCE, MeatGrindingRecipe.Type.INSTANCE);
+    }
+
+    private static <T extends Recipe<SimpleInventory>> void registerRecipe(String serId, String typeId, RecipeSerializer<T> serInstance, RecipeType<T> typeInstance) {
+        Registry.register(Registries.RECIPE_SERIALIZER, new Identifier(Tywragriculture.MOD_ID, serId),
+                serInstance);
+        Registry.register(Registries.RECIPE_TYPE, new Identifier(Tywragriculture.MOD_ID, typeId),
+                typeInstance);
     }
 }
